90937f193a484e08a24aa77888da84ee384f6905
Daniel Cassimiro <daniel.c.da.silva@accenture.com>
2020-04-30 11:24:38 -0300

    Listando todas categorias com dto

--- src/main/java/com/daniel/cursomc/services/CategoriaService.java
+++ src/main/java/com/daniel/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.daniel.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,7 +41,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível ecluir uma categoria que possui produtos");
 		}
-		
-		
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
