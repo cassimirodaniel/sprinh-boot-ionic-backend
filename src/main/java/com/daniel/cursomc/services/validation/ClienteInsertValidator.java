23ac6290bd3e9246a2409474dab3f743cd4fff22
Daniel Cassimiro <daniel.c.da.silva@accenture.com>
2020-05-05 20:05:05 -0300

    Email personalizado e validação de email

--- src/main/java/com/daniel/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/daniel/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.daniel.cursomc.domain.Cliente;
 import com.daniel.cursomc.domain.enums.TipoCliente;
 import com.daniel.cursomc.dto.ClienteNewDTO;
+import com.daniel.cursomc.repositories.ClienteRepository;
 import com.daniel.cursomc.resources.exception.FieldMessage;
 import com.daniel.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -28,7 +35,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
 		}
-
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existe"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
