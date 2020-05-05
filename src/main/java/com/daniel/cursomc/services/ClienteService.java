9db722a47289843f42f5651515e2f0267af44922
Daniel Cassimiro <daniel.c.da.silva@accenture.com>
2020-05-05 19:50:47 -0300

    Validacao customizada de CPF e CNPJ na insercao de Cliente

--- src/main/java/com/daniel/cursomc/services/ClienteService.java
+++ src/main/java/com/daniel/cursomc/services/ClienteService.java
@@ -74,7 +74,7 @@ public class ClienteService {
 	}
 	
 	public Cliente fromDTO(ClienteNewDTO objDTO) {
-		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfouCnpj(),
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(),
 				TipoCliente.toEnum(objDTO.getTipo()));
 		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
 		
